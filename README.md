5d6e4db5cd19aa44febd334d631d4e95f2516f7b
Ezekiel Abolade <ezekielabolade@gmail.com>
2022-06-30 16:34:32 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Frontend Mentor - IP address tracker solution
+
+This is my solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0).
+
+### Screenshot
+
+![](./images/screenshot.png)
+
+### Links
+
+- Solution URL: [Check solution]()
+- Live Site URL: [Visit live site](https://thefaceiptracker.vercel.app)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- JavaScript
+
+## Author
+
+- Website - [Ezekiel The Face](https://thefaceportfolio.netlify.app)
+- Frontend Mentor - [@iamtheface](https://www.frontendmentor.io/profile/iamtheface)
+- Twitter - [@thefacetweets](https://www.twitter.com/thefacetweets)
---
+++ script.js
@@ -0,0 +1,52 @@
+var api_key = "at_ZK89x7V16ZmKS0A0Kz41qO0eRPhv7";
+var map = L.map("maparea", {
+  zoomControl: false,
+});
+document.querySelector("form").addEventListener("submit", GetLocation);
+
+function GetLocation() {
+  var ip = document.querySelector("input").value;
+  try {
+    fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${api_key}&ipAddress=${ip}`
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong.");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        const IP = document.querySelector(".ipaddress");
+        const Location = document.querySelector(".location");
+        const Timezone = document.querySelector(".timezone");
+        const isp = document.querySelector(".isp");
+        IP.innerHTML = data.ip;
+        Location.innerHTML =
+          data.location.region + ", " + data.location.country;
+        Timezone.append(" " + data.location.timezone);
+        isp.innerHTML = data.isp;
+        const lat = data.location.lat
+        const long = data.location.lng
+        map.setView([lat, long], 14)
+
+      })
+      .catch((err) => {
+        document.querySelector(".container").innerHTML =
+          "<h2>" + err.message + "</h2>";
+        console.log(err.message);
+      });
+  } catch {
+    console.log(error);
+  }
+  document.querySelector("input").value = ""
+}
+
+GetLocation();
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
+}).addTo(map);
